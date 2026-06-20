72bde33fbb6f61df4c0c26073f75305d2eaf1f0f
SUJAY KUMAR M <sujay.manoharan200@gmail.com>
2026-06-20 18:16:17 +0530

    Add files via upload

---
+++ task 2 --js/task 2.js
@@ -0,0 +1,9 @@
+let number = -10;
+
+if (number > 0) {
+    console.log("The number is Positive");
+} else if (number < 0) {
+    console.log("The number is Negative");
+} else {
+    console.log("The number is Zero");
+}
